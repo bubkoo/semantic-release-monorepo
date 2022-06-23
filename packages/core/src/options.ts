57c77b4ff14771f3ab2a1ee1c9b618d2d223210a
bubkoo <bubkoo.wy@gmail.com>
2022-06-23 11:48:57 +0800

    fix: 🐛 fix template error

--- packages/core/src/options.ts
+++ packages/core/src/options.ts
@@ -64,15 +64,31 @@ export function getSuccessComment(footer?: string) {
 }
 
 export function getFailComment(footer?: string) {
-  // const HOME_URL = 'https://github.com/semantic-release/semantic-release'
-  // const FAQ_URL = `${HOME_URL}/blob/caribou/docs/support/FAQ.md`
-  // const GET_HELP_URL = `${HOME_URL}#get-help`
-  // const USAGE_DOC_URL = `${HOME_URL}/blob/caribou/docs/usage/README.md`
-  // const NEW_ISSUE_URL = `${HOME_URL}/issues/new`
+  const HOME_URL = 'https://github.com/semantic-release/semantic-release'
+  const FAQ_URL = `${HOME_URL}/blob/caribou/docs/support/FAQ.md`
+  const GET_HELP_URL = `${HOME_URL}#get-help`
+  const USAGE_DOC_URL = `${HOME_URL}/blob/caribou/docs/usage/README.md`
+  const NEW_ISSUE_URL = `${HOME_URL}/issues/new`
   const footerMsg = footer || DEFAULT_FOOTER
-  return `
+  return `## :rotating_light: The automated release from the <%= branch.name %> branch failed. :rotating_light:
+
+I recommend you give this issue a high priority, so other packages depending on you can benefit from your bug fixes and new features again.
+
+You can find below the list of errors reported by **semantic-release**. Each one of them has to be resolved in order to automatically publish your package. I'm sure you can fix this 💪.
+
+Errors are usually caused by a misconfiguration or an authentication problem. With each error reported below you will find explanation and guidance to help you to resolve it.
+
+Once all the errors are resolved, **semantic-release** will release your package the next time you push a commit to the <%= branch.name %> branch. You can also manually restart the failed CI job that runs **semantic-release**.
+
+If you are not sure how to resolve this, here are some links that can help you:
+- [Usage documentation](${USAGE_DOC_URL})
+- [Frequently Asked Questions](${FAQ_URL})
+- [Support channels](${GET_HELP_URL})
+
+If those don't help, or if this issue is reporting something you think isn't right, you can always ask the humans behind **[semantic-release](${NEW_ISSUE_URL})**.
+
 <% var formatError = function (error) { %>
-  <% var header = '### ' + error.message + '\n\n' %>
+  <% var header = '### ' + error.message + '\\n\\n' %>
   <% if(error.details) { %>
     <% return header + error.details %>
   <% } else { %>
